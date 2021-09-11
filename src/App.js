5bf2ab70e0da3b05c901598687d771fcc3ba8410
Geoffrey <gs9675a@student.american.edu>
2021-09-10 22:51:30 -0400

    added shop, header, and contact pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import ContactPage from './pages/contact/contact.component'
+import Header from './components/header/header.component'
 
 const HatsPage = () => (
   <div>
@@ -16,9 +18,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/contact' component={ContactPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ContactPage = () => (
+    <div className='contact-info'>
+        <h3>Geoffrey's Contact info</h3>
+        <p>Does this render</p>
+    </div>
+)
+
+export default ContactPage
