5f03d8b867569d90afb15b163f3294951b345fe6
Geoffrey <gs9675a@student.american.edu>
2021-09-30 23:45:07 -0400

    git

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import ContactPage from './pages/contact/contact.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component'
+import { auth } from './firebase/firebase.utils'
+import reactDom from 'react-dom';
 
 const HatsPage = () => (
   <div>
@@ -15,17 +18,45 @@ const HatsPage = () => (
 )
 
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor () {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user })
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+
+  render() {
+    return (
     <div>
-      <Header />
+      <Header currentUser={this.state.currentUser}/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUp}/>
         <Route exact path='/contact' component={ContactPage} />
       </Switch>
     </div>
   );
+  }
+
+  
 }
 
 export default App;
